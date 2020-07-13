a02a8983b41453393ffc1cde2a262a3543dcb6b7
OptimusPrimeRib <alex.s.burman@gmail.com>
2020-07-13 22:31:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#import pygame
+#print('Haia world!')
+#player_age = int(input('how old are you???'))
+#print('hahahaha you fool i now know your age ' + player_age + ' you ignorant fool!')
+#if player_age < 14:
+#  print('oooo so young lol')
+#else: print('thats wow for you C:')
+#player_name = input('whats your name?')
+#print('well hello there' + player_name)
+
+
+#if 'dog' < 'gecko':
+#  print('pandas are fatttt')
+
+green = 0
+yellow = 4
+le_number = input('enter any number')
+if int(le_number) < 2:
+  green = yellow
+if green == yellow:
+  print('raspberry')
+else:
+  print('orange')
+number = input('enter any number')
+if int(number) % 2 == 0:
+  print('number is even')
+else:
+  print('number is odd')
+
+for i in range(17):
+  print(i)
+
+
+
+
+  n = 10
+  while n >= 1:
+    print(n)
+    n=n-1
